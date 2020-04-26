8306e4720e337418211ef2cce74f84088e7faaca
t-gergely <t-gergely@users.noreply.github.com>
2020-04-27 00:40:33 +0200

    Redmine 4.1

--- lib/redmine_user_specific_theme.rb
+++ lib/redmine_user_specific_theme.rb
@@ -5,14 +5,6 @@ require_dependency 'redmine_user_specific_theme/hooks/view_hooks'
 require_dependency 'redmine_user_specific_theme/patches/user_preference_patch'
 require_dependency 'redmine_user_specific_theme/patches/application_helper_patch'
 
-ActiveSupport::Reloader.to_prepare do
-
-  unless UserPreference.included_modules.include?(RedmineUserSpecificTheme::Patches::UserPreferencePatch)
-    UserPreference.send :prepend, RedmineUserSpecificTheme::Patches::UserPreferencePatch
-  end
-
-  unless ApplicationHelper.included_modules.include?(RedmineUserSpecificTheme::Patches::ApplicationHelperPatch)
-    ApplicationHelper.send :include, RedmineUserSpecificTheme::Patches::ApplicationHelperPatch
-  end
-
-end
+UserPreference.prepend(RedmineUserSpecificTheme::Patches::UserPreferencePatch)
+ApplicationHelper.prepend(RedmineUserSpecificTheme::Patches::ApplicationHelperPatch)
+ActionView::Base.prepend(ApplicationHelper)
--- lib/redmine_user_specific_theme/patches/application_helper_patch.rb
+++ lib/redmine_user_specific_theme/patches/application_helper_patch.rb
@@ -1,24 +1,16 @@
-require 'application_helper'
-
-module RedmineUserSpecificTheme::Patches
-  module ApplicationHelperPatch
-    extend ActiveSupport::Concern
+module RedmineUserSpecificTheme::Patches:: ApplicationHelperPatch
 
     def current_theme
-      user_theme = super
       user_theme = Redmine::Themes.theme(User.current.pref.ui_theme)
-      user_theme || Redmine::Themes.theme(Setting.ui_theme)
+      user_theme || super
     end
 
     def body_css_classes
       css_classes = super
-      css_classes = body_css_classes_without_user_specific
       user_theme = Redmine::Themes.theme(User.current.pref.ui_theme)
       user_theme ?
           css_classes.gsub(/theme-\S+/, "theme-#{user_theme.name}") :
           css_classes
     end
 
-  end
 end
-Redmine::Themes.prepend(RedmineUserSpecificTheme::Patches::ApplicationHelperPatch)
